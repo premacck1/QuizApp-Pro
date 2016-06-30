fcf398e0da502efd9a2eb9bfd7a3e88bc9bca1c2
Prem $ <premsuman8@gmail.com>
2016-06-30 21:24:05 +0530

    please check Questions activity as it selects multiple choices at a time. I've also made a Custom Adapter (CustomAdapter.class) which populates listView in questions Activity

--- app/src/main/java/com/dikcoder/prem/quizapp/Questions.java
+++ app/src/main/java/com/dikcoder/prem/quizapp/Questions.java
@@ -1,20 +1,41 @@
 package com.dikcoder.prem.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class Questions extends AppCompatActivity {
 
+    ListView optionList;
     static String ARGS = "questionArgs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
-        /*
-        ListView optionList = (ListView) this.findViewById(R.id.optionsList);
 
-        */
+        optionList = (ListView) this.findViewById(R.id.optionsList);
+
+        String[] choices = {
+                "Option 1",
+                "Option 2",
+                "Option 3",
+                "Option 4"
+        };
+
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), choices);
+        optionList.setAdapter(customAdapter);
+/*
+        optionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setSelected(true);
+            }
+        });
+*/
+
+
 
 /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -29,4 +50,9 @@ public class Questions extends AppCompatActivity {
 */
     }
 
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(Questions.this, MainActivity.class));
+        super.onBackPressed();
+    }
 }
